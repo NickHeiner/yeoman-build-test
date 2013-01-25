9977a56e8391512f824b3026eaade8f3f11dc8c5
Nick Heiner <nick.heiner@opower.com>
2013-01-25 15:53:25 -0500

    Initial import of generated yeoman project

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var yeomanBuildTestApp = angular.module('yeomanBuildTestApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+yeomanBuildTestApp.controller('MainCtrl', function($scope) {
+  $scope.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Testacular'
+  ];
+});
